2d515cf0b29efaa0c7b8644478b19ebd2f4d2266
Krissaane Inès <38727492+ineskris@users.noreply.github.com>
2019-07-03 00:06:33 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,12 +11,12 @@ shinyUI(pageWithSidebar(
                 sliderInput("V", "V_0:", 
                               value=-75, min=-100, max=100, step=1),
                 sliderInput("n", "n_0:", 
-                              value=0.325, min=-10, max=10, step=0.1),
+                              value=0.325, min=0, max=1, step=0.1),
                 sliderInput("m", "m_0:", 
-                              value=0.05, min=-10, max=10, step=0.1),
+                              value=0.05, min=0, max=1, step=0.1),
                 
                 sliderInput("h", "h_0:", 
-                              value=0.6, min=-10, max=10, step=0.1)),
+                              value=0.6, min=0, max=1, step=0.1)),
              
         wellPanel(
           tags$h4("Constants"), 
@@ -27,11 +27,11 @@ shinyUI(pageWithSidebar(
         tags$h5("Conductances"),
                   
                   sliderInput("S", "Sodium g_Na:", 
-                              value=120, min=1, max=200, step=1),
+                              value=120, min=-10, max=100, step=1),
                   sliderInput("P", "Potassium g_K:", 
-                              value=36, min=1, max=200, step=1),
+                              value=36, min=-10, max=100, step=1),
                   sliderInput("L", "Leak g_L:", 
-                              value=0.3, min=0, max=10, step=0.1),
+                              value=0.3, min=-10, max=100, step=0.1),
     
        tags$h5("Equilibrium potentials"),
                  
@@ -57,7 +57,7 @@ shinyUI(pageWithSidebar(
                               choices=list("mS")),
                   conditionalPanel(condition="input.timex == 'mS'",
                                    sliderInput("tmax", "Time max:",
-                                               value=200, min=1, max=500, step=10))),
+                                               value=200, min=1, max=1000, step=10))),
         
     
 
@@ -84,7 +84,6 @@ shinyUI(pageWithSidebar(
                             ))
                               ))
 ))
-    
 
 
 
